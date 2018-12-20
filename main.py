53ac356294683c47b4bfe4b4f067190405e7310b
Mehdi Goudarzi <mmhdgd@gmail.com>
2018-12-20 22:51:13 +0000

    First commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# importing other scripts
+import config
+import pandas as pd
+import numpy as np
+
+
+def function_b():
+    print("LOL")
+
+
+def function_a():
+    print(config.a)
+
+
+if __name__ == '__main__':
+    function_a()
